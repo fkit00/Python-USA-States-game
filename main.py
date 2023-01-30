bc1be6be1269ea4e240de2808e8d2c60884a7b53
fiona <fiona.kitchen@yahoo.co.uk>
2023-01-30 15:40:50 +0000

    moving onto pandas

--- main.py
+++ main.py
@@ -5,5 +5,8 @@
 import csv
 with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
+    temps=[]
     for row in data: 
-        print(row)
+        if row[1] != "temp":
+            temps.append(int(row[1]))
+    print(temps)
